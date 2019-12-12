d8445cbd318ab5eaed9cc2c1fd4b0a87dfa83757
Raghu Betina <raghu@firstdraft.com>
2019-12-12 06:04:21 +0000

    Only relationship user can modify relationship

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,14 @@
 class RelationshipsController < ApplicationController
+  before_action :current_user_must_be_relationship_home_owner, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_relationship_home_owner
+    relationship = Relationship.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == relationship.home_owner
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @relationships = Relationship.all
 
