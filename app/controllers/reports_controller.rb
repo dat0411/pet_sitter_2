eb2f56de47aaa474a9c077eb64bebd0f9470340e
Raghu Betina <raghu@firstdraft.com>
2019-12-12 06:04:39 +0000

    Install Kaminari

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    @reports = Report.all
+    @reports = Report.page(params[:page]).per(10)
 
     render("report_templates/index.html.erb")
   end
